fac8a0aab50266f8ab510cff329bf47e139d540b
Aaron Hsieh <zz85721@gmail.com>
2024-08-30 00:00:34 +0800

    Add the case switch and TODO

---
+++ main.py
@@ -0,0 +1,19 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, x, /): ").strip()
+
+
+if operation == "+":
+    # TODO
+    # print(Add(a, b))
+elif operation == "-":
+    # TODO
+    # print(Minus(a, b))
+elif operation == "x":
+    # TODO
+    # print(Multiply(a, b))
+elif operation == "/":
+    # TODO
+    # print(Divide(a, b))
+else:
+    print("Error")
